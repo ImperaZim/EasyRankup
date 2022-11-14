2c32117a5649feb9da669a0a3c4421c5a95e3587
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-13 22:29:25 -0300

    Create ChatGroupEvent.php

---
+++ src/ImperaZim/EasyGroups/Events/ChatGroupEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ImperaZim\EasyGroups\Events;
+
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use ImperaZim\EasyGroups\Loader;  
+use pocketmine\event\player\PlayerChatEvent;
+use ImperaZim\EasyGroups\Functions\Groups\_Group;  
+
+class ChatGroupEvent implements Listener {
+
+ public function Event(PlayerChatEvent $event) {
+  $player = $event->getPlayer();
+  $message = $event->getMessage();
+  if(!$player->hasPermission("chat.colored.format")){
+   $colors = ["§", "&"];
+   $message = str_replace($colors, [""], $message);
+  }
+  $plugin = Loader::getInstance();
+  $config = new Config($plugin->getDataFolder() . "groups.yml"); 
+  $name = $player->getName();
+  $group = _Group::get($player);
+  $tag = $config->getAll()[$group]["tag"] ?? "[]";
+  $chat = $config->getAll()[$group]["chat"] ?? "{tag} {name}: {message}";
+  $format = Loader::getProcessedTags(["{tag}", "{name}", "{message}"], [$tag, $name, $message], $chat);
+  $event->setFormat($format);
+ }
+
+}  
