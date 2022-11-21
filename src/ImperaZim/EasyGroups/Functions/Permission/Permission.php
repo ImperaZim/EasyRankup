50846007c9f07a4e33946265708026d18b652667
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-21 01:41:22 -0300

    Update Permission.php

--- src/ImperaZim/EasyGroups/Functions/Permission/Permission.php
+++ src/ImperaZim/EasyGroups/Functions/Permission/Permission.php
@@ -8,7 +8,7 @@ use pocketmine\permissions\DefaultPermissionNames;
 
 class Permission {
  
- const CORE_PERM = "\x70\x70\x65\x72\x6d\x73\x2e\x63\x6f\x6d\x6d\x61\x6e\x64\x2e\x70\x70\x69\x6e\x66\x6f"; 
+ const CORE_PERM = "pocketmine"; 
  const USER_PERM = "pocketmine.group.user";
  const OPERATOR_PERM = "pocketmine.group.operator";
  const USER_BROADCAST = "pocketmine.broadcast.user";
