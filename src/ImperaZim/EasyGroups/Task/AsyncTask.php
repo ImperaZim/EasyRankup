ecae01019ebb207c24f5805eccdd5fb12efe8105
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-12-05 13:24:02 -0300

    Update AsyncTask.php

--- src/ImperaZim/EasyGroups/Task/AsyncTask.php
+++ src/ImperaZim/EasyGroups/Task/AsyncTask.php
@@ -6,13 +6,27 @@ use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\scheduler\Task;
 use ImperaZim\EasyGroups\Loader;
+use pocketmine\scheduler\CancelTaskException; 
 use ImperaZim\EasyGroups\Functions\Groups\_Group; 
 use ImperaZim\EasyGroups\Functions\Storage\SQLite3;
 use ImperaZim\EasyGroups\Functions\Groups\UpdateGroup; 
 
 class AsyncTask extends Task {
 
+ public $state = 0;
+ 
+ public static function register($loader, AsyncTask $task) : void {
+  $loader->getScheduler()->scheduleRepeatingTask($task, 10);
+ }
+  
+ public function unregister() : void {
+  $this->state = 1;
+ }
+
  public function onRun() : void {
+  if ($this->state != 0) {
+   throw new CancelTaskException();
+  } 
   $plugin = Loader::getInstance();
   $server = Server::getInstance();
   $config = new Config($plugin->getDataFolder() . "groups.yml");
