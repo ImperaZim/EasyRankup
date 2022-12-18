e14cfca0d5d11fcdefc8a916a9dee6d941b4aea7
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-12-18 16:39:37 -0300

    update EasyEconomy.php

--- src/ImperaZim/EasyRankup/Dependence/EasyEconomy.php
+++ src/ImperaZim/EasyRankup/Dependence/EasyEconomy.php
@@ -3,19 +3,25 @@
 namespace ImperaZim\EasyRankup\Dependence;
 
 use pocketmine\player\Player; 
+use ImperaZim\EasyRankup\EasyRankup;
 
-class EasyEconomy extends \pocketmine\Server {
+class EasyEconomy {
+ 
+ public $economy = null;
+ 
+ public function __construct() {
+  $economy = Loader::getInstance()->getServer()->getPluginManager()->getPlugin("EasyEconomy");
+  $this->economy = $economy; 
+ } 
  
  public static function getMoney(Player $player) {
-  $manager = self::getInstance()->getPluginManager();
-  if ($manager->getPlugin("EasyEconomy") == null) return 0;
-  return $manager->getPlugin("EasyEconomy")->getProvinder()->getMoney($player);
+  if ($this->economy == null) return 0;
+  return $this->economy->getProvinder()->getMoney($player);
  }
  
  public static function reduceMoney(Player $player, Int $value) {
-  $manager = self::getInstance()->getPluginManager();
-  if ($manager->getPlugin("EasyEconomy") == null) return;
-  return $manager->getPlugin("EasyEconomy")->getProvinder()->reduceMoney($player, $value);
+  if ($this->economy == null) return;
+  return $this->economy->getProvinder()->reduceMoney($player, $value);
  }
  
 }
