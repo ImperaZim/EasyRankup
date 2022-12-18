96d4b2f814354baa4376d549724b496e20cc6ec3
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-12-18 16:21:06 -0300

    Add files via upload

---
+++ src/ImperaZim/EasyRankup/Functions/RankManager/RankManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ImperaZim\EasyRankup\Functions\RankManager; 
+
+use pocketmine\player\Player;
+
+class RankManager extends Rank {
+ 
+ public function __construct() {
+  return $this;
+ }
+ 
+ public function getId(Player $player) : string {
+  return $this->getRankId($player);
+ }
+ 
+ public function getRank(Player $player) : string {
+  return $this->getRankName($player);
+ }
+ 
+ public function getTag(Player $player) : string {
+  return $this->getTagByRank($this->getRank($player));
+ }
+ 
+ public function getPriceToUpgrade(Player $player) : Int {
+  return $this->getPriceByRank($this->getRankNameById($this->getId($player) + 1));
+ }
+ 
+ public function getNextRank(Player $player) : string {
+  return $this->getTagByRank($this->getRankNameById($this->getId($player) + 1));
+ }
+ 
+}
---
+++ src/ImperaZim/EasyRankup/Functions/RankManager/RankPermission.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ImperaZim\EasyRankup\Functions\RankManager;
+
+use ImperaZim\EasyRankup\EasyRankup;
+
+class RankPermission extends RankManager {
+ 
+ public function __construct() {
+  return $this;
+ }
+
+ public function UpdatePermissions($player) : void {
+  $plugin = Easyrankup::getInstance(); 
+
+  foreach ($this->getAll() as $rank => $data) {
+   foreach ($data["permissions"] as $perm) {
+    $player->addAttachment($plugin)->setPermission($perm, false);
+   }
+  }
+
+  foreach ($this->getPermissionsByRank($this->getRank($player)) as $perm) {
+   $player->addAttachment($plugin)->setPermission($perm, true);
+  }
+
+ }
+
+}
+ 
