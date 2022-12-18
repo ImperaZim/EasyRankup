c6360ef26a67a508f4739e818f724ccae48382c7
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-12-18 16:41:00 -0300

    update EasyEconomy.php

--- src/ImperaZim/EasyRankup/Dependence/EasyEconomy.php
+++ src/ImperaZim/EasyRankup/Dependence/EasyEconomy.php
@@ -7,21 +7,21 @@ use ImperaZim\EasyRankup\EasyRankup;
 
 class EasyEconomy {
  
- public $economy = null;
+ public static $economy = null;
  
  public function __construct() {
-  $economy = Loader::getInstance()->getServer()->getPluginManager()->getPlugin("EasyEconomy");
-  $this->economy = $economy; 
+  $economy = EasyRankup::getInstance()->getServer()->getPluginManager()->getPlugin("EasyEconomy");
+  self::$economy = $economy; 
  } 
  
  public static function getMoney(Player $player) {
-  if ($this->economy == null) return 0;
-  return $this->economy->getProvinder()->getMoney($player);
+  if (self::$economy == null) return 0;
+  return self::$economy->getProvinder()->getMoney($player);
  }
  
  public static function reduceMoney(Player $player, Int $value) {
-  if ($this->economy == null) return;
-  return $this->economy->getProvinder()->reduceMoney($player, $value);
+  if (self::$economy == null) return;
+  return self::$economy->getProvinder()->reduceMoney($player, $value);
  }
  
 }
