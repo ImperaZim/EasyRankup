65d744a3efd3c2210415a1d17bc0d7c2c9b98483
Impera <92953608+ImperaZim@users.noreply.github.com>
2023-01-05 14:50:14 -0300

    Create PluginUtils.php

---
+++ src/ImperaZim/EasyGroups/PluginUtils.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ImperaZim\EasyGroups;
+
+class PluginUtils {
+ 
+ public function replace($tags, $tostring, $message) : string {
+  $message = str_replace(["{prefix}"], [EasyGroups::getInstance()->getMessages()->getNested("plugin.perfix")], $message);
+  return str_replace($tags, $tostring, $message);
+ }  
+ 
+} 
